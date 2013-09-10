17652a32d2c48984e2c0667471d1073af2fdaccc
Kathryn G Turner <kathryn.g.turner@gmail.com>
2013-09-09 17:35:26 -0700

    making climate table, a nicer way!

--- Extract_climate_data.R
+++ Extract_climate_data.R
@@ -1,281 +1,76 @@
 #to make table of climate data from WorldClim.org tiles
 
 #load packages: raster, rgdal, sp
+library(rgdal)
+library(raster)
+# library(sp)
+library(foreach)
 
-#load files for one tile
-tile12bioclim1 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio1_12.tif")
-hasValues(tile12bioclim1)
-#[1] TRUE
-inMemory(tile12bioclim1)
-#[1] FALSE
-#repeat for all other tiffs
-#tile12
-tile12bioclim2 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio2_12.tif")
-hasValues(tile12bioclim2)
-inMemory(tile12bioclim2)
-tile12bioclim3 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio3_12.tif")
-hasValues(tile12bioclim3)
-inMemory(tile12bioclim3)
-tile12bioclim4 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio4_12.tif")
-hasValues(tile12bioclim4)
-inMemory(tile12bioclim4)
-tile12bioclim5 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio5_12.tif")
-hasValues(tile12bioclim5)
-inMemory(tile12bioclim5)
-tile12bioclim6 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio6_12.tif")
-hasValues(tile12bioclim6)
-inMemory(tile12bioclim6)
-tile12bioclim7 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio7_12.tif")
-hasValues(tile12bioclim7)
-inMemory(tile12bioclim7)
-tile12bioclim8 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio8_12.tif")
-hasValues(tile12bioclim8)
-inMemory(tile12bioclim8)
-tile12bioclim9 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio9_12.tif")
-hasValues(tile12bioclim9)
-inMemory(tile12bioclim9)
-tile12bioclim10 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio10_12.tif")
-hasValues(tile12bioclim10)
-inMemory(tile12bioclim10)
-tile12bioclim11 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio11_12.tif")
-hasValues(tile12bioclim11)
-inMemory(tile12bioclim11)
-tile12bioclim12 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio12_12.tif")
-hasValues(tile12bioclim12)
-inMemory(tile12bioclim12)
-tile12bioclim13 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio13_12.tif")
-hasValues(tile12bioclim13)
-inMemory(tile12bioclim13)
-tile12bioclim14 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio14_12.tif")
-hasValues(tile12bioclim14)
-inMemory(tile12bioclim14)
-tile12bioclim15 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio15_12.tif")
-hasValues(tile12bioclim15)
-inMemory(tile12bioclim15)
-tile12bioclim16 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio16_12.tif")
-hasValues(tile12bioclim16)
-inMemory(tile12bioclim16)
-tile12bioclim17 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio17_12.tif")
-hasValues(tile12bioclim17)
-inMemory(tile12bioclim17)
-tile12bioclim18 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio18_12.tif")
-hasValues(tile12bioclim18)
-inMemory(tile12bioclim18)
-tile12bioclim19 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_12\\bio19_12.tif")
-hasValues(tile12bioclim19)
-inMemory(tile12bioclim19)
-#tile13
-tile13bioclim1 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio1_13.tif")
-hasValues(tile13bioclim1)
-inMemory(tile13bioclim1)
-tile13bioclim2 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio2_13.tif")
-hasValues(tile13bioclim2)
-inMemory(tile13bioclim2)
-tile13bioclim3 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio3_13.tif")
-hasValues(tile13bioclim3)
-inMemory(tile13bioclim3)
-tile13bioclim4 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio4_13.tif")
-hasValues(tile13bioclim4)
-inMemory(tile13bioclim4)
-tile13bioclim5 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio5_13.tif")
-hasValues(tile13bioclim5)
-inMemory(tile13bioclim5)
-tile13bioclim6 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio6_13.tif")
-hasValues(tile13bioclim6)
-inMemory(tile13bioclim6)
-tile13bioclim7 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio7_13.tif")
-hasValues(tile13bioclim7)
-inMemory(tile13bioclim7)
-tile13bioclim8 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio8_13.tif")
-hasValues(tile13bioclim8)
-inMemory(tile13bioclim8)
-tile13bioclim9 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio9_13.tif")
-hasValues(tile13bioclim9)
-inMemory(tile13bioclim9)
-tile13bioclim10 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio10_13.tif")
-hasValues(tile13bioclim10)
-inMemory(tile13bioclim10)
-tile13bioclim11 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio11_13.tif")
-hasValues(tile13bioclim11)
-inMemory(tile13bioclim11)
-tile13bioclim12 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio12_13.tif")
-hasValues(tile13bioclim12)
-inMemory(tile13bioclim12)
-tile13bioclim13 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio13_13.tif")
-hasValues(tile13bioclim13)
-inMemory(tile13bioclim13)
-tile13bioclim14 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio14_13.tif")
-hasValues(tile13bioclim14)
-inMemory(tile13bioclim14)
-tile13bioclim15 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio15_13.tif")
-hasValues(tile13bioclim15)
-inMemory(tile13bioclim15)
-tile13bioclim16 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio16_13.tif")
-hasValues(tile13bioclim16)
-inMemory(tile13bioclim16)
-tile13bioclim17 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio17_13.tif")
-hasValues(tile13bioclim17)
-inMemory(tile13bioclim17)
-tile13bioclim18 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio18_13.tif")
-hasValues(tile13bioclim18)
-inMemory(tile13bioclim18)
-tile13bioclim19 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_13\\bio19_13.tif")
-hasValues(tile13bioclim19)
-inMemory(tile13bioclim19)
-#tile16
-tile16bioclim1 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio1_16.tif")
-hasValues(tile16bioclim1)
-inMemory(tile16bioclim1)
-tile16bioclim2 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio2_16.tif")
-hasValues(tile16bioclim2)
-inMemory(tile16bioclim2)
-tile16bioclim3 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio3_16.tif")
-hasValues(tile16bioclim3)
-inMemory(tile16bioclim3)
-tile16bioclim4 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio4_16.tif")
-hasValues(tile16bioclim4)
-inMemory(tile16bioclim4)
-tile16bioclim5 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio5_16.tif")
-hasValues(tile16bioclim5)
-inMemory(tile16bioclim5)
-tile16bioclim6 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio6_16.tif")
-hasValues(tile16bioclim6)
-inMemory(tile16bioclim6)
-tile16bioclim7 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio7_16.tif")
-hasValues(tile16bioclim7)
-inMemory(tile16bioclim7)
-tile16bioclim8 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio8_16.tif")
-hasValues(tile16bioclim8)
-inMemory(tile16bioclim8)
-tile16bioclim9 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio9_16.tif")
-hasValues(tile16bioclim9)
-inMemory(tile16bioclim9)
-tile16bioclim10 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio10_16.tif")
-hasValues(tile16bioclim10)
-inMemory(tile16bioclim10)
-tile16bioclim11 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio11_16.tif")
-hasValues(tile16bioclim11)
-inMemory(tile16bioclim11)
-tile16bioclim12 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio12_16.tif")
-hasValues(tile16bioclim12)
-inMemory(tile16bioclim12)
-tile16bioclim13 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio13_16.tif")
-hasValues(tile16bioclim13)
-inMemory(tile16bioclim13)
-tile16bioclim14 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio14_16.tif")
-hasValues(tile16bioclim14)
-inMemory(tile16bioclim14)
-tile16bioclim15 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio15_16.tif")
-hasValues(tile16bioclim15)
-inMemory(tile16bioclim15)
-tile16bioclim16 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio16_16.tif")
-hasValues(tile16bioclim16)
-inMemory(tile16bioclim16)
-tile16bioclim17 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio17_16.tif")
-hasValues(tile16bioclim17)
-inMemory(tile16bioclim17)
-tile16bioclim18 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio18_16.tif")
-hasValues(tile16bioclim18)
-inMemory(tile16bioclim18)
-tile16bioclim19 <- raster("~\\grad work\\Courses\\KH_microarray\\bio_16\\bio19_16.tif")
-hasValues(tile16bioclim19)
-inMemory(tile16bioclim19)
+######Read files named xyz1111.csv, xyz2222.csv, etc. from http://stackoverflow.com/questions/5319839/read-multiple-csv-files-into-separate-data-frames
+filenames <- list.files(path="~/grad work/Centaurea diffusa/WorldClim_2013/")
 
-#make a vector for each tile
-tile12tiffvector<-c(tile12bioclim1, tile12bioclim2, tile12bioclim3, tile12bioclim4,tile12bioclim5,tile12bioclim6,tile12bioclim7,tile12bioclim8,tile12bioclim9,tile12bioclim10,tile12bioclim11,tile12bioclim12,tile12bioclim13,tile12bioclim14,tile12bioclim15,tile12bioclim16,tile12bioclim17,tile12bioclim18,tile12bioclim19)
-tile13tiffvector<-c(tile13bioclim1, tile13bioclim2, tile13bioclim3, tile13bioclim4,tile13bioclim5,tile13bioclim6,tile13bioclim7,tile13bioclim8,tile13bioclim9,tile13bioclim10,tile13bioclim11,tile13bioclim12,tile13bioclim13,tile13bioclim14,tile13bioclim15,tile13bioclim16,tile13bioclim17,tile13bioclim18,tile13bioclim19)
-tile16tiffvector<-c(tile16bioclim1, tile16bioclim2, tile16bioclim3, tile16bioclim4,tile16bioclim5,tile16bioclim6,tile16bioclim7,tile16bioclim8,tile16bioclim9,tile16bioclim10,tile16bioclim11,tile16bioclim12,tile16bioclim13,tile16bioclim14,tile16bioclim15,tile16bioclim16,tile16bioclim17,tile16bioclim18,tile16bioclim19)
+# ##Create list of data frame names without the ".csv" part 
+# names <-substr(filenames,1,120)
 
-#make empty vector for values
-valuevectorPop1<-NULL
-valuevectorPop2<-NULL
-valuevectorPop3<-NULL
-valuevectorPop4<-NULL
-valuevectorPop5<-NULL
-valuevectorPop6<-NULL
-valuevectorPop7<-NULL
-valuevectorPop8<-NULL
-valuevectorPop9<-NULL
-valuevectorPop10<-NULL
-valuevectorPop11<-NULL
-valuevectorPop12<-NULL
-
-#load each collection site as SpatialPoints
-popdata<-read.table(file.choose(), header=T, sep="") #collection location information
-#longitude, latitude!!
-Pop1<-SpatialPoints(as.matrix(t(c(-95.958333,44.32536))))
-Pop1<-SpatialPoints(as.matrix(t(c(popdata[1,4], popdata[1,3]))))
-#rest of the pops
-Pop2<-SpatialPoints(as.matrix(t(c(popdata[2,4], popdata[2,3]))))
-Pop3<-SpatialPoints(as.matrix(t(c(popdata[3,4], popdata[3,3]))))
-Pop4<-SpatialPoints(as.matrix(t(c(popdata[4,4], popdata[4,3]))))
-Pop5<-SpatialPoints(as.matrix(t(c(popdata[5,4], popdata[5,3]))))
-Pop6<-SpatialPoints(as.matrix(t(c(popdata[6,4], popdata[6,3]))))
-Pop7<-SpatialPoints(as.matrix(t(c(popdata[7,4], popdata[7,3]))))
-Pop8<-SpatialPoints(as.matrix(t(c(popdata[8,4], popdata[8,3]))))
-Pop9<-SpatialPoints(as.matrix(t(c(popdata[9,4], popdata[9,3]))))
-Pop10<-SpatialPoints(as.matrix(t(c(popdata[10,4], popdata[10,3]))))
-Pop11<-SpatialPoints(as.matrix(t(c(popdata[11,4], popdata[11,3]))))
-Pop12<-SpatialPoints(as.matrix(t(c(popdata[12,4], popdata[12,3]))))
-
-#which tile is the population on? check pops 5 and 6 for tile13 and pop 8 for tile15
-extract(tile16bioclim1, Pop8)
-
-#for loop for one collection site, creating value vector
-for(map in tile12tiffvector){
-  myValue<-extract(map, Pop1)
-  valuevectorPop1<-c(valuevectorPop1, myValue)
-}
-#repeat for all pops, tile12
-for(map in tile12tiffvector){
-  myValue<-extract(map, Pop2)
-  valuevectorPop2<-c(valuevectorPop2, myValue)
-}
-for(map in tile12tiffvector){
-  myValue<-extract(map, Pop3)
-  valuevectorPop3<-c(valuevectorPop3, myValue)
-}
-for(map in tile12tiffvector){
-  myValue<-extract(map, Pop4)
-  valuevectorPop4<-c(valuevectorPop4, myValue)
-}
-#for loops, tile13
-for(map in tile13tiffvector){
-  myValue<-extract(map, Pop5)
-  valuevectorPop5<-c(valuevectorPop5, myValue)
-}
-for(map in tile13tiffvector){
-  myValue<-extract(map, Pop6)
-  valuevectorPop6<-c(valuevectorPop6, myValue)
-}
-#for loops, tile16
-for(map in tile16tiffvector){
-  myValue<-extract(map, Pop7)
-  valuevectorPop7<-c(valuevectorPop7, myValue)
-}
-for(map in tile16tiffvector){
-  myValue<-extract(map, Pop8)
-  valuevectorPop8<-c(valuevectorPop8, myValue)
-}
-for(map in tile16tiffvector){
-  myValue<-extract(map, Pop9)
-  valuevectorPop9<-c(valuevectorPop9, myValue)
-}
-for(map in tile16tiffvector){
-  myValue<-extract(map, Pop10)
-  valuevectorPop10<-c(valuevectorPop10, myValue)
-}
-for(map in tile16tiffvector){
-  myValue<-extract(map, Pop11)
-  valuevectorPop11<-c(valuevectorPop11, myValue)
-}
-for(map in tile16tiffvector){
-  myValue<-extract(map, Pop12)
-  valuevectorPop12<-c(valuevectorPop12, myValue)
+###Load all files
+for(i in filenames){
+  filepath <- file.path("~/grad work/Centaurea diffusa/WorldClim_2013/",i)
+  assign(i, raster(filepath))
 }
 
-#check length of all vectors
-length(valuevectorPop4)
+###check all files http://stackoverflow.com/questions/15387727/use-object-names-as-list-names-in-r
+list <- mget(filenames, envir=globalenv())
+for(i in list){
+  if (hasValues(i)==FALSE){
+    print(i,"hasValues error") 
+  }
+  if (inMemory(i)==TRUE){
+    print(i, "inMemory error")
+  }
+  else{
+    print("All checked out!")
+  }
+}
+
+#pop coord
+allpop <- read.table(file.choose(), header=T, sep="\t") #Popcoord.txt
+Frpop <- allpop[allpop$Pop %in% Frdes$Pop,]
+rownames(Frpop) <- Frpop$Pop
+Frpop$Pop <- droplevels(Frpop$Pop)
+
+#load pop coord at SpatialPoints
+for(i in Frpop$Pop){
+  assign(i,SpatialPoints(as.matrix(t(c(Frpop[i,2], Frpop[i,1])))))
+}
+
+#make table
+poplist <- mget(levels(Frpop$Pop), envir=globalenv())
+
+tiffvector <- unlist(list)
+
+climate <- foreach(t=tiffvector, .combine='cbind') %:%    
+  foreach(p=poplist, .combine='rbind') %do%{
+    myValue<-extract(t, p)
+  } #may take a while
+
+#tidy table
+Frclim <- as.data.frame(climate)
+row.names(Frclim) <- Frpop$Pop
+colnames(Frclim) <- filenames
+
+#need to merge columns so that all bio1 columns are merged, etc. 
+#Should have total of 20 columns (19 bioclim variables + altitude)
+
+#merge two columns
+# ## Copy BNR.y if BNR.x is missing
+# d$BNR.x[is.na(d$BNR.x)] <- d$BNR.y[is.na(d$BNR.x)]
+# ## List the indices of BNR.x that are still missing
+# which(is.na(d$BNR.x))
+# ## List the indices where BNR.x is different from BNR.y
+# which(d$BNR.x != d$BNR.y)
+
+# ####
+
 #join value vectors into table
 KHbioclim<-rbind(valuevectorPop1,valuevectorPop2,valuevectorPop3,valuevectorPop4,valuevectorPop5,valuevectorPop6,valuevectorPop7,valuevectorPop8,valuevectorPop9,valuevectorPop10,valuevectorPop11,valuevectorPop12)
 KHbioclim<-as.data.frame(KHbioclim)
