b18e74806acb9f535afa9864ffbf426c567f5718
Kathryn G Turner <kathryn.g.turner@gmail.com>
2014-02-08 16:05:06 -0800

    Redo pca and remake frdat (think about what to remake/redo... especially add SK and before converting dates and/or long transforming)

--- Fr_climate_pca.R
+++ Fr_climate_pca.R
@@ -2,7 +2,7 @@
 
 #add lat/long
 #get population coordinates
-allpop <- read.table(file.choose(), header=T, sep="\t") #Popcoord.txt !not wordlclim approximations
+allpop <- read.table("Popcoord.txt", header=T, sep="\t") #Popcoord.txt !not wordlclim approximations
 Frdes <- read.table("Frdes.txt", header=T, sep="\t")
 Frpop <- allpop[allpop$Pop %in% Frdes$Pop,]
 rownames(Frpop) <- Frpop$Pop
@@ -17,10 +17,10 @@ row.names(Frclimdat) <- Frclimdat$Pop
 Frclim.pca <- prcomp(Frclimdat[2:23], center=TRUE, scale=TRUE)
 summary(Frclim.pca)
 # Importance of components:
-#                         PC1    PC2    PC3    PC4    PC5     PC6    
-# Standard deviation     2.4796 2.3822 2.0296 1.5433 1.0808 0.85425 
-# Proportion of Variance 0.2795 0.2580 0.1872 0.1083 0.0531 0.03317 
-# Cumulative Proportion  0.2795 0.5374 0.7247 0.8329 0.8861 0.91922 
+#                           PC1    PC2    PC3    PC4     PC5     PC6     PC7
+# Standard deviation     2.5058 2.3797 2.1050 1.5462 1.14469 0.93389 0.77103
+# Proportion of Variance 0.2854 0.2574 0.2014 0.1087 0.05956 0.03964 0.02702
+# Cumulative Proportion  0.2854 0.5428 0.7442 0.8529 0.91247 0.95211 0.97914
 
 #visualize components
 plot(Frclim.pca)
@@ -37,12 +37,17 @@ Frclimdat2 <- cbind(Frclimdat, PC1, PC2, PC3, PC4)
 #find top loadings
 loadings <- Frclim.pca$rotation[,1]
 sort(abs(loadings))
-# alt      bio18      bio15      bio10      bio14       bio1       bio2  Longitude       bio8   Latitude      bio17       bio5 
-# 0.00995386 0.04658053 0.05829867 0.10981148 0.11420187 0.12110800 0.12526569 0.12956648 0.14417015 0.14941215 0.15001422 0.15302580 
-# bio3       bio9      bio11       bio6      bio13      bio16      bio12       bio7      bio19       bio4 
-# 0.15873725 0.20161735 0.24932707 0.27877950 0.28224989 0.28734473 0.31565663 0.33815842 0.35222244 0.35643172 
+# alt        bio2       bio10       bio17   Longitude        bio5       bio12 
+# 0.001561833 0.041357958 0.090877697 0.105026831 0.112019618 0.115751459 0.135513808 
+# bio14       bio13       bio16        bio8       bio15       bio18    Latitude 
+# 0.138930963 0.164047194 0.176494679 0.184740915 0.186201310 0.213953960 0.220288560 
+# bio7       bio19        bio3        bio1        bio4        bio6        bio9 
+# 0.227854928 0.254227563 0.262227179 0.266800012 0.322945390 0.326315353 0.332758344 
+# bio11 
+# 0.340912515 
 #reverse order, top ones at the end. Lat 13th.
-#profound or mundane? bio7, bio19, bio4 also top three loadings for kay's ragweed
+#profound or mundane? bio7, bio19, bio4 also top three loadings for kay's ragweed.
+#hahaha or bug in code!
 
 Frclimdat2 <- cbind(Frclimdat2, Frclimdat$bio4, Frclimdat$bio19, Frclimdat$bio7)
 
