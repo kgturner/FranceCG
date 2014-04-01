b78631b40dffb47ec540d2bd03ed921a28972f9b
Kathryn G Turner <kathryn.g.turner@gmail.com>
2014-03-31 17:46:30 -0700

    add phenology and harvest data. Some problems with mass...

---
+++ Frdeath_format.R
@@ -0,0 +1,77 @@
+#Fr_death
+#format for mixed lmer
+#March 2014
+ 
+library(lme4)
+library(lsmeans)
+library(ggplot2)
+library(plyr)
+
+#data formating
+#dates
+frdeath<- read.table("Frdeath.txt", header=T, sep="\t",quote='"')
+
+frdeath$Bolt.date2 <- strptime(frdeath$Bolt.date, format="%B %d, %Y")
+frdeath$Bolt.date2 <- as.Date(frdeath$Bolt.date2)
+day0 <- as.Date("2011-05-12")
+frdeath$Bolt.date3 <- as.numeric(frdeath$Bolt.date2-day0)
+frdeath$Bolt.date <- frdeath$Bolt.date3
+frdeath <- frdeath[,1:13]
+
+frdeath$Harvest.date2 <- strptime(frdeath$Harvest.date, format="%B %d, %Y")
+frdeath$Harvest.date2 <- as.Date(frdeath$Harvest.date2)
+day0 <- as.Date("2011-05-12")
+frdeath$Harvest.date3 <- as.numeric(frdeath$Harvest.date2-day0)
+frdeath$Harvest.date <- frdeath$Harvest.date3
+frdeath <- frdeath[,1:13]
+
+frdeath$Death.date2 <- strptime(frdeath$Death.date, format="%B %d, %Y")
+frdeath$Death.date2 <- as.Date(frdeath$Death.date2)
+day0 <- as.Date("2011-05-12")
+frdeath$Death.date3 <- as.numeric(frdeath$Death.date2-day0)
+frdeath$Death.date <- frdeath$Death.date3
+frdeath <- frdeath[,1:13]
+
+frdeath$Yellow2 <- strptime(frdeath$Yellow, format="%B %d, %Y")
+frdeath$Yellow2 <- as.Date(frdeath$Yellow2)
+day0 <- as.Date("2011-05-12")
+frdeath$Yellow3 <- as.numeric(frdeath$Yellow2-day0)
+frdeath$Yellow <- frdeath$Yellow3
+frdeath <- frdeath[,1:13]
+
+frdeath$TotWilt2 <- strptime(frdeath$TotWilt, format="%B %d, %Y")
+frdeath$TotWilt2 <- as.Date(frdeath$TotWilt2)
+day0 <- as.Date("2011-05-12")
+frdeath$TotWilt3 <- as.numeric(frdeath$TotWilt2-day0)
+frdeath$TotWilt <- frdeath$TotWilt3
+frdeath <- frdeath[,1:13]
+
+frdeath$Wilt2 <- strptime(frdeath$Wilt, format="%B %d, %Y")
+frdeath$Wilt2 <- as.Date(frdeath$Wilt2)
+day0 <- as.Date("2011-05-12")
+frdeath$Wilt3 <- as.numeric(frdeath$Wilt2-day0)
+frdeath$Wilt <- frdeath$Wilt3
+frdeath <- frdeath[,1:13]
+
+# #bolt.bin
+# Frdatsk.l$bolt.bin <- as.numeric(Frdatsk.l$BoltedatH)-1
+# head(Frdatsk.l[Frdatsk.l$Origin=="sk",])
+
+#write
+write.table(frdeath, file="Frdeath.txt",sep="\t", quote=F)
+#read
+frdeath<- read.table("Frdeath.txt", header=T, sep="\t",quote='"', row.names=1)
+
+#add to Frdatsk
+setdiff(FrdatSK$tagged, frdeath$tagged)
+
+# frdeath$tagged <- as.character(frdeath$tagged)
+# frdeath[343,3] <- "US022-3"
+# frdeath$tagged <- as.factor(frdeath$tagged)
+
+setdiff(frdeath$tagged, FrdatSK$tagged)
+subset(frdeath, tagged%in%setdiff(frdeath$tagged, FrdatSK$tagged)&Trt==c("control", "drought")&Ending=="H")
+
+
+#read
+FrdatSK<- read.table("FrTraitClimDat_SK.txt", header=T, sep="\t",quote='"', row.names=1)
